dddbaabb49f3b1f6e5b788b0ac0fdd1bb15a802e
Rodrigo Teixeira <rodrigo@simplesdental.com>
2017-07-11 13:23:35 -0300

    Fix location where get path

--- runAgainSavedPorts.sh
+++ runAgainSavedPorts.sh
@@ -5,14 +5,14 @@ while [ "$port" -le "$portmax" ]; do
     echo "Verifing $port..."
     if grep -q "/stage -Dhttp.port=$port" /tmp/running-ports.txt
         then
-             echo "exist $port"
-             allPath=`egrep -o "(?/opt.*)(?$port)" running-ports.txt`
+             echo "$port was running"
+             allPath=`egrep -o "(?/opt.*)(?$port)" /tmp/running-ports.txt`
              path=`echo $allPath | sed "s/\/target\/universal\/stage \-Dhttp.port=$port//g"`
              echo $path
              sudo start-play $port $path &
         else
-            #echo "not exist"
-        fi
+            echo "$port wasn’t running"
+    fi
     port=$(( port + 1 ))
 done
 rm /tmp/running-ports.txt
